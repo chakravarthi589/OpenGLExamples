9e06c21a8183baaa5972539c2edb879dab4e6df9
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-15 14:40:59 +0900

    Create GL_QUADS.cpp

---
+++ src/C++/GeometricPrimitives/GL_QUADS.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "include/GL/freeglut.h"
+
+void display()
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.5, 0.0);
+		glVertex2f(0.5, 0.5);
+		glVertex2f(0.0, 0.5);
+	glEnd();
+
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(640, 640);
+	glutCreateWindow("OpenGL - GL_QUADS Demo");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
