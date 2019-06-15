aaa231096130fa7ca8c611ae7a51ff9aa8d4c161
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-15 11:09:51 +0900

    Create SolidTeapot.cpp

---
+++ src/C++/GeometricPrimitives/SolidTeapot.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "include/GL/freeglut.h"
+
+void display()
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glutSolidTeapot(0.5);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(640, 640);
+	glutCreateWindow("OpenGL - Solid Teapot Demo");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
