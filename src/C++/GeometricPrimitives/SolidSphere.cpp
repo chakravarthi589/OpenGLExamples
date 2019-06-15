3871a47e45cb0d4a54c8eb5485f63c0fe798e0e8
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-15 10:59:04 +0900

    Update SolidSphere.cpp

--- src/C++/GeometricPrimitives/SolidSphere.cpp
+++ src/C++/GeometricPrimitives/SolidSphere.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "include/GL/freeglut.h"
 
+void display()
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glutSolidSphere(0.5, 50, 50);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(640, 640);
+	glutCreateWindow("OpenGL - Solid Sphere Demo");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
