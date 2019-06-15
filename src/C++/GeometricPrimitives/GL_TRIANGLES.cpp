8943f4031d6e96ff45393fdcb6bc637beffe84a4
Bharatesh Chakravarthi S B <chakravarthi589@gmail.com>
2019-06-15 14:16:13 +0900

    Create GL_TRIANGLES.cpp

---
+++ src/C++/GeometricPrimitives/GL_TRIANGLES.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "include/GL/freeglut.h"
+
+void display()
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.5, 0.0);
+		glVertex2f(0.0, 0.5);
+	glEnd();
+
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(640, 640);
+	glutCreateWindow("OpenGL - GL_TRIANGLES Demo");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
